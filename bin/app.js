c36921c9bce8e18537bf92b9098dfce6adc3cb6a
mzaini30 <muhzaini30@gmail.com>
2018-06-13 21:24:39 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,3 +1,24 @@
+// buat fungsi baru
+
+jQuery.fn.center = function () {
+    this.css("position","absolute")
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px")
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + "px")
+    return this
+}
+
+// cover
+
+$('.cover').height($(window).height()).width($(window).width())
+$('.cover-isi').center()
+$('.navbar, .isi-aplikasi').hide()
+$('.cover').click(function(){
+	$(this).slideUp()
+	$('.navbar, .isi-aplikasi').show()
+})
+
+// isi aplikasi
+
 $('.arab p').hide()
 lihat_menu = function(){
 	if ($('.menu-cek').is(':checked')){
